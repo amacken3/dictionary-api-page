14a5158cc92bfbf3eb53e1774f2b6380b8e55d9b
Aengus MacKenzie <aengus.2000@gmail.com>
2026-03-20 13:59:04 -0400

    error shown when invalid input. Clears if valid input is put in.

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const searchForm = document.getElementById("search-form");
 const wordInput = document.getElementById("word-input");
 const resultsSection = document.getElementById("results-section");
 const resultsDiv = document.getElementById("results");
+const DICTIONARY_API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 searchForm.addEventListener("submit", handleSearch);
-
+//search event
 async function handleSearch(event) {
     event.preventDefault();
 
     const word = getInputValue();
+    const isValidInput = validateInput(word);
+    clearResults();
+
+    if (!isValidInput) {
+        showResults();
+        displayError("Please enter a valid word.");
+        return;
+    }
     console.log(word);
 
 }
@@ -18,15 +27,20 @@ function getInputValue() {
 }
 
 function validateInput(word) {
-
+    
+    if (word.trim().length === 0) {
+        return false;
+    } 
+    
+    return true;
 }
 
 function clearResults() {
-
+    resultsDiv.textContent = "";
 }
 
 function showResults() {
-
+    resultsSection.style.display = "block";
 }
 
 function hideResults() {
@@ -34,7 +48,7 @@ function hideResults() {
 }
 
 function displayError(message) {
-
+    resultsDiv.textContent = message;
 }
 
 function fetchWordData(word) {
